82aa018dce1838a4bae94c56346a3b0d8122502d
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-02-05 10:57:49 +0800

    creating all the components i need for the project and track the change of the input file.

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,12 @@
+//import React, {useState} from 'react'
+import React from 'react'
+import { TodoForm } from './TodoForm'
+
+export const TodoWrapper = () => {
+    //const [todo, setTodo] = useState([])
+  return (
+    <div className='TodoWrapper'>
+        <TodoForm/>
+    </div>
+  )
+}
