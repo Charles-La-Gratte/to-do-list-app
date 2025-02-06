05e772b711c2860c5b5e6f6bbf1fb24b41507e0c
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-02-06 13:05:03 +0800

    Workinng on the form, add Todos and using props, marking Todos as completed

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,17 +1,21 @@
 import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
     const [value, setValue] = useState("")
     const handleSubmit = e => {
         //useState is for tracking the change on the input
         //the input change is saved in setValue
-        //to prevent the reload of the page in case
+        //to prevent the reload of the page in case because if you submit the form the page reload automatically
         e.preventDefault();
-        console.log(value);      
+        addTodo(value); 
+        //after i submit the form i want to clear it the input
+        setValue("")
+
+        
     }
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type="text" className='todo-input' placeholder='What is the task today ?' onChange={(e) => setValue(e.target.value)}/>
+        <input type="text" value={value} className='todo-input' placeholder='What is the task today ?' onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
 
