5c4cb2c1ea8390596656227b73f17b1901258705
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-02-07 16:04:28 +0800

    work on editing and deleting task

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
 import { Todo } from './Todo';
+import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -11,16 +12,33 @@ export const TodoWrapper = () => {
       setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false }])
       console.log(todos)
     }
+    //function to mark the task completed or not
     const toggleComplete = id => {
       setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
+    }
+    //function for deleting the selected task if i click to the trash icon
+    const deleteTodo = id => {
+      setTodos(todos.filter(todo => todo.id !== id ))
+    }
+    //function for editing the selected task if i click to edit icon
+    const editTodo = id => {
+      setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+    }
+    const editTask = (task, id) => {
+      setTodos(todos.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
     }
   return (
     <div className='TodoWrapper'>
       <h1>Get things done !!!</h1>
         <TodoForm addTodo={addTodo}/>
         {todos.map((todo,index) => (
-          <Todo task={todo} key={index}
-          toggleComplete={toggleComplete}/>
+          todo.isEditing ? (
+            <EditTodoForm editTodo={editTask} task={todo}/>
+          ) : (
+            <Todo task={todo} key={index}
+            toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo}/>
+          )
+          
         ))}
         
     </div>
