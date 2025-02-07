866af3309dd14de104c8723046ed026cb19d55b2
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-02-07 16:00:18 +0800

    work on editTodoForm and deleteForm

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task)
+    const handleSubmit = e => {
+        //useState is for tracking the change on the input
+        //the input change is saved in setValue
+        //to prevent the reload of the page in case because if you submit the form the page reload automatically
+        e.preventDefault();
+        editTodo(value, task.id); 
+        //after i submit the form i want to clear it the input
+        setValue("")
+
+        
+    }
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type="text" value={value} className='todo-input' placeholder='Update your task here' onChange={(e) => setValue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
+
   )
 }
