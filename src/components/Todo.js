86d1a56c9c09190106e0e9f66651ca5c8ad5ed94
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-02-09 15:43:43 +0800

    add the liked buttom and do the animations for the icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { motion } from "framer-motion"
+
 
 export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  const [isLiked, setIsLiked] = useState(false)
+
+  const colorTodoHeart = () => {
+    setIsLiked (!isLiked)
+  }
   return (
     <div className='Todo'>
       <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? "completed" : ""}`}>{task.task}</p>
       <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+        <motion.div style={{ display: "inline-block" }} whileHover={{ scale: 1.5 }} whileTap={{ scale: 0.9 }}>  
+          <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+        </motion.div> 
+
+        <motion.div style={{ display: "inline-block" }} whileHover={{ scale: 1.5 }} whileTap={{ scale: 0.9 }}> 
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+        </motion.div> 
+
+        <motion.div style={{ display: "inline-block" }} whileTap={{ scale: 1.3, rotate: 50 }} whileHover={{ scale: 1.5 }} transition={{ type: "spring", stiffness: 200 }}> 
+          <FontAwesomeIcon icon={faHeart} onClick={colorTodoHeart} style={{ color: isLiked ? "red" : "white", cursor: "pointer" }} />
+        </motion.div>
       </div>
     </div>
   )
